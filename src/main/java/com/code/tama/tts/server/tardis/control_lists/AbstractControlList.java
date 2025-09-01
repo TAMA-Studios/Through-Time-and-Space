20f3257c44b0d4d5aa34c88691278833eb5deb73
Codiak540 <tomo42806@gmail.com>
2025-09-01 03:10:29 +0000

    start work on new control system for consoles

--- src/main/java/com/code/tama/tts/server/tardis/control_lists/AbstractControlList.java
+++ src/main/java/com/code/tama/tts/server/tardis/control_lists/AbstractControlList.java
@@ -6,4 +6,9 @@ import net.minecraft.world.phys.Vec3;
 
 public abstract class AbstractControlList {
     public abstract HashMap<Vec3, Vec3> GetPositionSizeMap();
+    private int ID = 0;
+    public int AddControl(int minX, int minY, int minZ, int maxX, int maxY, int maxZ){
+        GetPositionSizeMap().add(minX, minY, minZ, maxX, maxY, maxZ);
+        return ID++;
+    }
 }
