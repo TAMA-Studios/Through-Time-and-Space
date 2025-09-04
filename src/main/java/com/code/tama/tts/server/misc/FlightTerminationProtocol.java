263bcb3a15c37fc82ed6aab63c409a64d9c608a9
Codiak540 <tomo42806@gmail.com>
2025-09-04 08:46:58 +0800

    Start work on a Flight Termination Protocol Builder and Registry

---
+++ src/main/java/com/code/tama/tts/server/misc/FlightTerminationProtocol.java
@@ -0,0 +1,20 @@
+package com.code.tama.tts.server.misc;
+
+import com.code.tama.tts.server.tardis.terminationprotocol.TerminationProtocol;
+import lombok.Builder;
+
+@Builder
+public class FlightTerminationProtocol {
+    // The probability of the TARDIS landing off course
+    public final float Accuracy;
+    // Whether the Pilot is able to select the termination protocol
+    public final boolean IsSelectable;
+    // How much the floaterior shakes during remat
+    public final float LandShakeAmount;
+    // Modifier used in calculations for how fast the TARDIS flies
+    public final float Speed;
+    // How much the exterior shakes during demat
+    public final float TakeoffShakeAmount;
+    public final String name;
+    private final TerminationProtocol terminationProtocol;
+}
