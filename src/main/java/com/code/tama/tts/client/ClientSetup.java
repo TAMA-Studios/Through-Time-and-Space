7376424b3148a23c87e8fa92d8320dfcd0b0cfd3
codiak540 <tomo42806@gmail.com>
2026-04-23 02:26:26 -0400

    not quite there yet but close

--- src/main/java/com/code/tama/tts/client/ClientSetup.java
+++ src/main/java/com/code/tama/tts/client/ClientSetup.java
@@ -95,6 +95,11 @@ public class ClientSetup {
 
 			// Register the shader instance with your handler
 			event.registerShader(shader, SkyBlock::setSkyShader);
+
+			TardisSkyEffects.registerShaders(
+					event.getResourceProvider(),
+                    event::registerShader
+			);
 		} catch (IOException ex) {
 			System.err.println("Failed to load shader");
 			ex.printStackTrace();
