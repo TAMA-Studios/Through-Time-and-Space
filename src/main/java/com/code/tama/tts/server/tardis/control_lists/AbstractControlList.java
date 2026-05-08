7a0c848177b13d1ba7ac05a3c070583e3b09d2c1
codiak540 <tomo42806@gmail.com>
2026-05-08 01:03:03 -0400

    not quite there yet but close

--- src/main/java/com/code/tama/tts/server/tardis/control_lists/AbstractControlList.java
+++ src/main/java/com/code/tama/tts/server/tardis/control_lists/AbstractControlList.java
@@ -17,18 +17,66 @@ public abstract class AbstractControlList {
 
 	public Map<Integer, AbstractControl> GetDefaultControlAssignment() {
 		return new HashMap<>();
-	};
+	}
 
 	public AbstractControlList() {
 	}
 
+	// No-rotation overloads for backwards compat
+
+	/** Min/max corners, no rotation. */
+	@Deprecated
+	public int AddControl(float minX, float minY, float minZ, float maxX, float maxY, float maxZ) {
+		return MinMaxAddControl(minX, minY, minZ, maxX, maxY, maxZ);
+	}
+
+	/** Min/max corners via vectors, no rotation. */
+	@Deprecated
 	public int AddControl(Vector3f min, Vector3f max) {
-		PositionSizeMap.add(new ControlEntityRecord(min.x, min.y, min.z, max.x, max.y, max.z, ID++));
+		return MinMaxAddControl(min.x, min.y, min.z, max.x, max.y, max.z);
+	}
+
+	// Rotation overloads (Blockbench exportS center + half-extents + yaw)
+
+	/**
+	 * Center position + half-extents + yaw. This is what the Blockbench plugin
+	 * should export.
+	 *
+	 * @param cx
+	 *            center X (relative to console origin)
+	 * @param cy
+	 *            center Y
+	 * @param cz
+	 *            center Z
+	 * @param hw
+	 *            half-width (X)
+	 * @param hh
+	 *            half-height (Y)
+	 * @param hd
+	 *            half-depth (Z)
+	 * @param yawDeg
+	 *            rotation around Y axis in degrees
+	 */
+	public int AddControl(float cx, float cy, float cz, float hw, float hh, float hd, float yawDeg) {
+		PositionSizeMap.add(new ControlEntityRecord(cx, cy, cz, hw, hh, hd, yawDeg, ID++));
 		return ID;
 	}
 
-	public int AddControl(float minX, float minY, float minZ, float maxX, float maxY, float maxZ) {
-		PositionSizeMap.add(new ControlEntityRecord(minX, minY, minZ, maxX, maxY, maxZ, ID++));
+	/** Center via vector + half-extents via vector + yaw. */
+	public int AddControl(Vector3f center, Vector3f halfExtents, float yawDeg) {
+		return AddControl(center.x, center.y, center.z, halfExtents.x, halfExtents.y, halfExtents.z, yawDeg);
+	}
+
+	// Internal: min/max corners -> center+half-extents (used by no-rotation
+	// overloads)
+	private int MinMaxAddControl(float minX, float minY, float minZ, float maxX, float maxY, float maxZ) {
+		float cx = (minX + maxX) / 2f;
+		float cy = (minY + maxY) / 2f;
+		float cz = (minZ + maxZ) / 2f;
+		float hw = (maxX - minX) / 2f;
+		float hh = (maxY - minY) / 2f;
+		float hd = (maxZ - minZ) / 2f;
+		PositionSizeMap.add(new ControlEntityRecord(cx, cy, cz, hw, hh, hd, (float) 0.0, ID++));
 		return ID;
 	}
 }
